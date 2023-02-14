25c1a668db7ec943971b041d36198e96f3dde568
narmadak <narmadak@gnrgy.com>
2023-02-14 21:42:19 +0530

    Add project files.

---
+++ scaffoldpractice/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace scaffoldpractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        viswaEntities context = new viswaEntities();
+        public ActionResult Index()
+
+        {
+            viswaEntities context = new viswaEntities();
+            var listofData = context.studentfiles.ToList();
+            return View(listofData);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(studentfile model)
+        {
+            context.studentfiles.Add(model);
+            context.SaveChanges();
+            ViewBag.Message = "Data Insert Successfully";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = context.studentfiles.Where(x => x.deptid == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(studentfile Model)
+        {
+            var data = context.studentfiles.Where(x => x.deptid == Model.stdid).FirstOrDefault();
+            if (data != null)
+            {
+                data.stdaddress = Model.stdaddress;
+                data.stdname = Model.stdname;
+                data.fees = Model.fees;
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("index");
+        }
+        public ActionResult Details(int id)
+        {
+            var data = context.studentfiles.Where(x => x.deptid == id).FirstOrDefault();
+            return View(data);
+        }
+        public ActionResult Delete(int id)
+        {
+            var data = context.studentfiles.Where(x => x.deptid == id).FirstOrDefault();
+            context.studentfiles.Remove(data);
+            context.SaveChanges();
+            ViewBag.Messsage = "Record Delete Successfully";
+            return RedirectToAction("index");
+        }
+
+
+
+    }
+}
---
+++ scaffoldpractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="scaffoldpractice.MvcApplication" Language="C#" %>
